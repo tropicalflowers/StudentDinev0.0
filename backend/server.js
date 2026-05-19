a9725e52a2c2e838a286da1414192e98f870e972
tanseem_kashyap <tanseemkashyap@gmail.com>
2026-05-19 13:08:31 +0530

    Update CORS origin to include Vercel app

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:8000',
+    origin: 'http://localhost:8000','https://studentdine.vercel.app/',
     methods: ['GET', 'POST'],
   },
 });
